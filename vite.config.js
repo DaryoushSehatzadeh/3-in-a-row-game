0ea12e895c16a55ab43336a0c35affc0c70247f3
DaryoushSehatzadeh <daryoush.sehatzadeh@gmail.com>
2025-02-11 21:54:41 -0400

    rebuilt for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/3-in-a-row-game/',
+  base: '/3-in-a-row-game/',
   plugins: [react()],
 })

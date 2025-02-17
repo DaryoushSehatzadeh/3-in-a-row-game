4c1dcd33fbca5259ef55647699943851da10a072
DaryoushSehatzadeh <daryoush.sehatzadeh@gmail.com>
2025-02-17 17:41:34 -0400

    Changes to main
    - Modified routes to account for base name

--- src/main.jsx
+++ src/main.jsx
@@ -9,11 +9,11 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router basename="/3-in-a-row-game">
+    <Router basename="/3-in-a-row-game/">
       <Routes>
         <Route path="/" element={<Home />}/>
-        <Route path="/sample" element={<App apiUrl={'https://prog2700.onrender.com/threeinarow/sample'}/>}/>
-        <Route path="/random" element={<App apiUrl={'https://prog2700.onrender.com/threeinarow/random'}/>}/>
+        <Route path="/3-in-a-row-game/sample" element={<App apiUrl={'https://prog2700.onrender.com/threeinarow/sample'}/>}/>
+        <Route path="/3-in-a-row-game/random" element={<App apiUrl={'https://prog2700.onrender.com/threeinarow/random'}/>}/>
       </Routes>
     </Router>
   </StrictMode>,

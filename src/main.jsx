22ccb00d30dc1bbbb3fbb8b76e110177b0003301
DaryoushSehatzadeh <daryoush.sehatzadeh@gmail.com>
2025-02-17 17:38:12 -0400

    Modified Index & main:
    - Changed icon in index
    - Changed title in index
    
    - Changed base route in main

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
+    <Router basename="/3-in-a-row-game">
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/sample" element={<App apiUrl={'https://prog2700.onrender.com/threeinarow/sample'}/>}/>
